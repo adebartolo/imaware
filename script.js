359d80331d60e71a74bcaaa44fb33045f63a95f7
Alexandra Debartolo <34449443+alexandradebartolo@users.noreply.github.com>
2019-08-21 23:55:18 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('hi');
+$('.carousel.carousel-slider').carousel({
+  // halfWidth: true,
+  indicators: true
+});
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+$(document).ready(function(){
+  $('.tooltipped').tooltip();
+});
